154f7a16ec4e305b268353591b0f2c22c6d0de16
Dave <dave@mymobile.info>
2023-09-27 14:43:58 -0700

    created

--- front/src/routes/printers/[printerId]/+page.svelte
+++ front/src/routes/printers/[printerId]/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+  export let data;
+
+</script>
+
 printer detail page
--- front/src/routes/printers/[printerId]/+page.ts
+++ front/src/routes/printers/[printerId]/+page.ts
@@ -1,11 +1,15 @@
 /** @type {import('./$types').PageLoad} */
 import { _apiUrl } from "../../+layout";
 
+
 export const load = async ({ fetch, params }) => {
-  let url = _apiUrl(`/v1/model/${params.modelId}`);
+  let url = _apiUrl(`/v1/printer/${params.printerId}`);
   let res = await fetch(url);
   if (!res.ok) {
     throw `Error while fetching data from ${url} (${res.status} ${res.statusText}).`;
   }
   const printer = await res.json();
+  console.log(printer)
+
+  return {printer}
 }
